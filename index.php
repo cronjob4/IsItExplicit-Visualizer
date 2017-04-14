b075a853ea2e6c288a7f439517ed66ecb3d5f419
Cole Fortson <cfortson1@gmail.com>
2017-04-14 16:01:15 -0500

    Added graph to index

--- index.php
+++ index.php
@@ -2,12 +2,62 @@
 <!doctype html>
 <html lang="en">
 
-<head>
+<?php
+
+include 'getData.php';
+
+//echo "graphing stuff";
+
+?>
+
+<style>
+div.container {
+    width: 100%;
+    border: 1px solid gray;
+}
+
+header, footer {
+    padding: 1em;
+    color: white;
+    background-color: black;
+    clear: left;
+    text-align: center;
+}
+
+nav {
+    float: left;
+    max-width: 160px;
+    margin: 0;
+    padding: 1em;
+}
+
+nav ul {
+    list-style-type: none;
+    padding: 0;
+}
+   
+nav ul a {
+    text-decoration: none;
+}
+
+article {
+    margin: auto;
+    width: 100%;
+    padding: 10px;
+}
+</style>
+
+<header>
     
+    
+    <h1>Song Name: <?php echo($songName); ?></h1>
     
 <script src="Chart.js/Chart.js"></script>
 
-<canvas id="myChart" width="400" height="400"></canvas>
+</header>
+
+<article>
+    <canvas id="myChart" width="100%" height="50"></canvas>
 <script>
 
 
@@ -15,7 +65,9 @@
 
 var ctx = document.getElementById("myChart");
 
-var data = {
+
+var myChart = new Chart(ctx, {
+    data: {
     datasets: [{
         data: [
             11,
@@ -40,12 +92,18 @@ var data = {
         "Grey",
         "Blue"
     ]
-};
-
-var myChart = new Chart(ctx, {
-    data: data,
+},
     type: 'polarArea',
-    options: options
+    options: {
+        responsive:true,
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
 });
 
 
@@ -53,12 +111,6 @@ var myChart = new Chart(ctx, {
     
 
 
-<?php
 
-include 'getData.php';
 
-//echo "graphing stuff";
-
-?>
-
-</head>
+</article>
