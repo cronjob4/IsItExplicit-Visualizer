1245b65a0ae80d6c4cbd192080b11dce956e5cdd
Ethan Phelps <phelpse@student.trinityvalleyschool.org>
2017-04-14 15:25:35 -0500

    Changed getData

--- getData.php
+++ getData.php
@@ -12,7 +12,7 @@ $baseURL = ("https://isitexplic.it/api?apikey=" . $apiKey . "&search=" . $search
 //  Initiate curl
 $ch = curl_init();
 // Disable SSL verification
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
 // Will return the response, if false it print the response
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 // Set the url
