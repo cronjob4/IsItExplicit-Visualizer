0119a59d5eb21df1aac8cbf722c2da04d7b7f6b7
Cole Fortson <cfortson1@gmail.com>
2017-04-19 11:19:00 -0500

    Cleaned up repo

--- index.php
+++ index.php
@@ -180,13 +180,6 @@
       <script type="text/javascript" src="assets/js/jquery-2.1.4.min.js?ver=1"></script>
       <!--<![endif]-->
       <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
-      <script type="text/javascript" src="assets/js/jquery.flexslider-min.js"></script>
-      <script type="text/javascript" src="assets/js/jquery.appear.js"></script>
-      <script type="text/javascript" src="assets/js/jquery.plugin.js"></script>
-      <script type="text/javascript" src="assets/js/jquery.countdown.js"></script>
-      <script type="text/javascript" src="assets/js/jquery.waypoints.min.js"></script>
-      <script type="text/javascript" src="assets/js/jquery.validate.min.js"></script>
-      <script type="text/javascript" src="assets/js/toastr.min.js"></script>
       <script type="text/javascript" src="assets/js/startuply.js"></script>
    </body>
    </html>
